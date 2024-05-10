3ab5b32e6b74c93ab32e38365a9d9eb3b5227a38
Felipe Vidal <felipevidal@sinaitechnologies.com>
2024-05-10 12:52:57 -0300

    Added tag rendering

--- apps/frontend/src/components/ProjectCard/ProjectCard.tsx
+++ apps/frontend/src/components/ProjectCard/ProjectCard.tsx
@@ -1,16 +1,42 @@
-import AlertCircle from "../../assets/AlertCircle";
+// import AlertCircle from "../../assets/AlertCircle";
 import ArrowUpRight from "../../assets/ArrowUpRight";
 import Atom01 from "../../assets/Atom01";
 import Building07 from "../../assets/Building07";
-import Globe04 from "../../assets/Globe04";
-import Telescope from "../../assets/Telescope";
+// import Globe04 from "../../assets/Globe04";
+// import Telescope from "../../assets/Telescope";
 
 export type ProjectCardType = {
   image?: string;
   name: string;
+  category: string;
+  modality: string;
+  tags: string[];
 };
 
-export const ProjectCard: React.FC<ProjectCardType> = ({ image, name }) => {
+interface TagButtonProps {
+  name: string;
+  color: string;
+  Icon?: React.ReactNode;
+}
+
+const TagButton: React.FC<TagButtonProps> = ({ name, color, Icon }) => {
+  const minW = name.length * 6;
+  
+  return (
+    <button className="cursor-pointer [border:none] p-0 bg-[transparent] flex flex-row items-start justify-start mix-blend-multiply">
+      <div className={`rounded-2xl bg-${color}-50 flex flex-row items-center justify-center py-0.5 pr-2 pl-1.5 gap-[4px]`}>
+        {Icon}
+        <div className={`relative text-xs leading-[18px] font-medium font-text-md-regular text-${color}-700 text-center inline-block min-w-[${minW}px]`}>
+          {name}
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export const ProjectCard: React.FC<ProjectCardType> = ({ image, name, tags, category, modality }) => {
+  const tagColors = ["blue-light", "pink", "orange", "blue-gray", "purple", "gray"];
+  
   return (
     <section className="mq1920:flex-1 mq768:flex-1 mq375:self-stretch rounded-lg flex flex-col items-center mq375:min-w-[280px] mq768:min-w-[323px] justify-start gap-[24px] mq1920:min-w-[406px] mq768:max-w-full mq1920:max-w-full text-left text-sm text-slateblue font-text-md-regular">
       <img
@@ -33,51 +59,11 @@ export const ProjectCard: React.FC<ProjectCardType> = ({ image, name }) => {
             </div>
           </div>
           <div className="self-stretch flex flex-row flex-wrap items-start justify-start py-0 mq375:pr-[81px] mq768:pr-[78px] pl-0 mq650:pr-[39px] mq1920:pr-[47px] mq650:box-border gap-[10px] min-h-[54px] text-center text-xs text-gray-700">
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] flex flex-row items-start justify-start mix-blend-multiply">
-              <div className="rounded-2xl bg-blue-light-50 flex flex-row items-center justify-center py-0.5 pr-2 pl-1.5 gap-[4px]">
-                <Building07 className="h-3 w-3 relative overflow-hidden shrink-0" />
-                <div className="relative text-xs leading-[18px] font-medium font-text-md-regular text-blue-light-700 text-center inline-block min-w-[60px]">
-                  Presencial
-                </div>
-              </div>
-            </button>
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] flex flex-row items-start justify-start mix-blend-multiply">
-              <div className="rounded-2xl bg-pink-50 flex flex-row items-center justify-center py-0.5 pr-2 pl-1.5 gap-[4px]">
-                <Atom01 className="h-3 w-3 relative overflow-hidden shrink-0" />
-                <div className="relative text-xs leading-[18px] font-medium font-text-md-regular text-pink-700 text-center inline-block min-w-[53px]">
-                  Extensão
-                </div>
-              </div>
-            </button>
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] flex flex-row items-start justify-start mix-blend-multiply">
-              <div className="rounded-2xl bg-orange-50 flex flex-row items-center justify-center py-0.5 pr-2 pl-1.5 gap-[4px]">
-                <AlertCircle className="h-3 w-3 relative overflow-hidden shrink-0" />
-                <div className="relative text-xs leading-[18px] font-medium font-text-md-regular text-orange-700 text-center inline-block min-w-[39px]">
-                  Inativo
-                </div>
-              </div>
-            </button>
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] flex flex-row items-start justify-start mix-blend-multiply">
-              <div className="rounded-2xl bg-purple-50 flex flex-row items-center justify-center py-0.5 pr-2 pl-1.5 gap-[4px]">
-                <Globe04 className="h-3 w-3 relative overflow-hidden shrink-0" />
-                <div className="relative text-xs leading-[18px] font-medium font-text-md-regular text-purple-700 text-center inline-block min-w-[57px]">
-                  Geografia
-                </div>
-              </div>
-            </button>
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] flex flex-row items-start justify-start mix-blend-multiply">
-              <div className="rounded-2xl bg-blue-gray-50 flex flex-row items-center justify-center py-0.5 pr-2 pl-1.5 gap-[4px]">
-                <Telescope className="h-3 w-3 relative overflow-hidden shrink-0" />
-                <div className="relative text-xs leading-[18px] font-medium font-text-md-regular text-blue-gray-700 text-center inline-block min-w-[66px]">
-                  Astronomia
-                </div>
-              </div>
-            </button>
-            <div className="hidden flex-row items-start justify-start mix-blend-multiply">
-              <div className="rounded-2xl bg-gray-100 flex flex-row items-center justify-center py-0.5 px-2">
-                <div className="relative leading-[18px] font-medium">Label</div>
-              </div>
-            </div>
+            <TagButton name={modality} color="pink" Icon={<Building07 className="h-3 w-3 relative overflow-hidden shrink-0 stroke-[#EE46BC]" />} />
+            <TagButton name={category} color="blue-light" Icon={<Atom01 className="h-3 w-3 relative overflow-hidden shrink-0 stroke-[#0BA5EC]" />} />
+            {tags.map((tag, index) => (
+              <TagButton key={index} name={tag} color={tagColors[index%tagColors.length]} />
+            ))}
           </div>
         </div>
       </div>
