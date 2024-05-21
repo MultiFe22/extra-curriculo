b8c4d8a800b5f2c79e884694352ab4a4360ca64c
Felipe Vidal <felipevidal@sinaitechnologies.com>
2024-05-21 14:38:14 -0300

    Adapted the rest of the project page to be responsive

--- apps/frontend/src/components/ProjectContent.tsx/ProjectContent.tsx
+++ apps/frontend/src/components/ProjectContent.tsx/ProjectContent.tsx
@@ -22,24 +22,24 @@ export const ProjectContent: React.FC = () => {
     "Pharetra morbi libero id aliquam elit massa integer tellus.\n";
 
   return (
-    <section className="self-stretch bg-gray-50 flex flex-col items-center justify-start pt-0 px-0 pb-16 box-border max-w-full text-left text-xl text-gray-900 font-text-md-regular">
-      <div className="self-stretch flex flex-col items-start justify-start py-0 px-4 box-border max-w-full">
-        <div className="self-stretch flex flex-col items-start justify-start max-w-full">
-          <div className="self-stretch flex flex-col items-start justify-start gap-[48px] max-w-full">
+    <section className="self-stretch bg-gray-50 flex flex-col items-center justify-start mq768:pt-0 mq768:px-0 mq768:pb-16 mq1920:py-24 mq1920:px-20 box-border max-w-full text-left mq768:text-xl mq1920:text-5xl text-gray-900 font-text-md-regular">
+      <div className="self-stretch flex flex-col mq768:items-start mq1920:items-center justify-start py-0 mq768:px-4 mq1920:px-8 box-border max-w-full">
+        <div className="self-stretch flex mq768:flex-col mq1920:flex-row items-start justify-start max-w-full mq1920:gap-[64px]">
+          <div className="mq768:self-stretch mq1920:flex-1 flex flex-col items-start justify-start gap-[48px] max-w-full">
             <ReactMarkdown
               children={markdownContent}
               remarkPlugins={[remarkGfm]}
               className="text-lg text-gray-600"
             />
 
-            <div className="self-stretch flex flex-col items-start justify-start gap-[12px] text-sm text-gray-600">
+            <div className="self-stretch mq1920:h-[25vw] flex flex-col items-start justify-start gap-[12px] text-sm text-gray-600">
               <img
-                className="self-stretch relative max-w-full overflow-hidden max-h-full object-cover"
+                className="self-stretch mq1920:flex-1 relative max-w-full overflow-hidden max-h-full object-cover"
                 alt=""
                 src="https://source.unsplash.com/a8JHGzB7h4A"
               />
             </div>
-            <div className="self-stretch flex flex-col items-start justify-start gap-[32px] text-base text-brand-600">
+            <div className="mq1920:hidden self-stretch flex flex-col items-start justify-start gap-[32px] text-base text-brand-600">
               <div className="self-stretch flex flex-col items-start justify-start gap-[24px]">
                 <div className="self-stretch relative leading-[24px] font-semibold">
                   Coordenação
@@ -90,6 +90,59 @@ export const ProjectContent: React.FC = () => {
                   </div>
                 </div>
               </footer>
+            </div>
+          </div>
+          <div className="mq768:hidden w-[280px] flex flex-col items-start justify-start gap-[32px] text-base text-brand-600 mq450:gap-[16px]">
+            <div className="h-px w-full bg-[#EAECF0] flex flex-col items-start justify-start relative max-w-full shrink-0" />
+            <div className="self-stretch flex flex-col items-start justify-start gap-[24px]">
+              <div className="self-stretch relative leading-[24px] font-semibold">
+                Coordenação
+              </div>
+              <div className="self-stretch flex flex-col items-start justify-start gap-[24px] text-gray-900">
+                <div className="flex flex-row items-center justify-start gap-[12px] mq450:flex-wrap">
+                  <Avatar className="h-12 w-12 relative rounded-181xl" />
+                  <div className="flex flex-col items-start justify-start">
+                    <div className="relative leading-[24px] font-semibold">
+                      Verônica Pinheiro Viana
+                    </div>
+                    <div className="relative leading-[24px] text-gray-600 inline-block min-w-[96px]">
+                      Responsável
+                    </div>
+                  </div>
+                </div>
+                <div className="self-stretch flex flex-row flex-wrap items-center justify-start gap-[12px]">
+                  <Avatar className="h-12 w-12 relative rounded-181xl" />
+                  <div className="flex-1 flex flex-col items-start justify-start min-w-[143px]">
+                    <div className="self-stretch relative leading-[24px] font-semibold">
+                      Instituto de Puericultura e Pediatria
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="h-px w-full bg-[#EAECF0] flex flex-col items-start justify-start relative max-w-full shrink-0" />
+            <div className="self-stretch flex flex-col items-start justify-start gap-[16px] text-gray-600">
+              <div className="self-stretch relative leading-[24px] font-semibold text-brand-600">
+                Endereço
+              </div>
+              <div className="self-stretch relative leading-[24px]">
+                R. Antônio Barros de Castro, 119 - Cidade Universitária, Rio de
+                Janeiro - RJ, 21941-853
+              </div>
+            </div>
+            <div className="h-px w-full bg-[#EAECF0] flex flex-col items-start justify-start relative max-w-full shrink-0" />
+            <div className="self-stretch flex flex-col items-start justify-start gap-[16px]">
+              <div className="self-stretch relative leading-[24px] font-semibold">
+                Contato
+              </div>
+              <div className="rounded-lg flex flex-row items-start justify-start text-gray-700">
+                <div className="shadow-[0px_1px_2px_rgba(16,_24,_40,_0.05)] rounded-lg bg-white overflow-hidden flex flex-row items-center justify-center py-2 px-[17px] gap-[8px] border-[1px] border-solid border-gray-300">
+                  <Mail01 className="h-5 w-5 relative overflow-hidden shrink-0 stroke-[#344054]" />
+                  <div className="relative leading-[24px] font-semibold whitespace-nowrap">
+                    contato@email.com
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
